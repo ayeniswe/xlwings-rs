615fd988284ee8602d4825fd428c0ea12a03c7eb
Samson Ayeni <ayenis@proton.me>
2025-02-15 17:18:31 -0600

    feat: add camelcase flag and rename flag

--- derive/src/enum_to_bytes.rs
+++ derive/src/enum_to_bytes.rs
@@ -2,10 +2,8 @@ use proc_macro::{Span, TokenStream};
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Ident, LitByteStr};
 
-/// Converts PascalCase enum variants to lowercase, or camelCase if needed.
-fn to_case_string(ident: &Ident) -> String {
-    let name = ident.to_string();
-    let mut chars = name.chars();
+fn to_camel_case(value: String) -> String {
+    let mut chars = value.chars();
     if let Some(first) = chars.next() {
         if first.is_uppercase() && chars.clone().any(|c| c.is_lowercase()) {
             // Convert first letter to lowercase for camelCase (e.g., AnExample -> anExample)
@@ -14,7 +12,7 @@ fn to_case_string(ident: &Ident) -> String {
             result
         } else {
             // Fully lowercase for standard cases (e.g., YEAR -> year)
-            name.to_lowercase()
+            value.to_lowercase()
         }
     } else {
         String::new()
@@ -28,36 +26,69 @@ fn create_lit_byte_str(value: String) -> LitByteStr {
 pub fn impl_enum_to_bytes(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
-
+    
+    // Check if all variants should be camelCase
+    let mut global_camel_case = false;
+    for attr in &input.attrs {
+        if attr.path().is_ident("camel") {
+            global_camel_case = true;
+        }
+    }
+    
     // Extract enum variants
     let data = match input.data {
         syn::Data::Enum(data) => data,
         _ => panic!("EnumToBytes can only be derived for enums"),
     };
 
-    let variants = data.variants.iter().map(|variant| {
+    let (try_from_variants, from_variants) = data.variants.iter().map(|variant| {
         let ident = &variant.ident;
-        let string_value = create_lit_byte_str(to_case_string(ident));
-        quote! {
-            #string_value => Ok(#name::#ident),
+        let mut ident_str = ident.to_string();
+        let mut rename = None;
+        let mut camel_case = globl_camel_case;
+        
+        // Get metadata to transform final name
+        for attr in &variant.attrs {
+            if attr.path().is_ident("name") {
+                rename = Some(attr.parse_args::<LitStr>().expect("expected a string for rename").value());
+            } else if attr.path().is_ident("camel") {
+                camel_case = true;
+            }
         }
-    });
-
-    let into_vec_cases = data.variants.iter().map(|variant| {
-        let ident = &variant.ident;
-        let string_value = create_lit_byte_str(to_case_string(ident));
-        quote! {
-            #name::#ident => #string_value.to_vec(),
+        
+        // Prevent using both rename and camelcase
+        if rename.is_some() && (camel_case || global_camel_case) {
+            return (
+                syn::Error::new_spanned(variant, "Cannot use both 'rename' and 'camelcase' attributes").to_compile_error(),
+                quote! {}
+            );
         }
-    });
-
+    
+        // Apply transformation
+        if let Some(rename) = rename {
+            ident_str = rename;
+        } else if camel_case {
+            ident_str = to_camel_case(ident_str);
+        }
+    
+        let lit_byte = create_lit_byte_str(ident_str);
+        (
+            quote! {
+                #lit_byte => Ok(#name::#ident),
+            },
+            quote! {
+                #name::#ident => #lit_byte.to_vec(),
+            }
+        )
+    }).unzip::<(Vec<_>, Vec<_>)>();
+    
     let expanded = quote! {
         impl TryFrom<Vec<u8>> for #name {
             type Error = XlsxError;
 
             fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
                 match value.as_slice() {
-                    #(#variants)*
+                    #(#try_from_variants)*
                     _ => {
                         let value = String::from_utf8_lossy(&value);
                         Err(XlsxError::MissingVariant(
@@ -72,7 +103,7 @@ pub fn impl_enum_to_bytes(input: TokenStream) -> TokenStream {
         impl From<#name> for Vec<u8> {
             fn from(value: #name) -> Self {
                 match value {
-                    #(#into_vec_cases)*
+                    #(#from_variants)*
                 }
             }
         }
